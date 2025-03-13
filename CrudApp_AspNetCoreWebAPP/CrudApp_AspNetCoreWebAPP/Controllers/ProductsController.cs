fbf4085a2b437af5a2c7616487d902e5c83734ab
Furqan-Ur-Rehman <furqanrehman653@gmail.com>
2025-03-13 16:18:18 +0500

    Upload to Consume Web API in Crud Web App

---
+++ CrudApp_AspNetCoreWebAPP/CrudApp_AspNetCoreWebAPP/Controllers/ProductsController.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+using CrudApp_AspNetCoreWebAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CrudApp_AspNetCoreWebAPP.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private string Base_Url = "https://localhost:7069/api/Products/";
+        private HttpClient Client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Products> products = new List<Products>();
+            HttpResponseMessage response = Client.GetAsync(Base_Url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Products>>(result);
+                if(data != null)
+                {
+                    products = data;
+                }
+            }
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Products products) 
+        { 
+            string data = JsonConvert.SerializeObject(products);
+            StringContent stringContent = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = Client.PostAsync(Base_Url, stringContent).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                TempData["Insert_Data"] = "Product has been Added!";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Products products = new Products();
+            HttpResponseMessage response = Client.GetAsync(Base_Url + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Products>(result);
+                if (data != null) 
+                {
+                    products = data;
+                }
+            }
+            return View(products);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Products products)
+        {
+            string data = JsonConvert.SerializeObject(products);
+            StringContent Content = new(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = Client.PutAsync(Base_Url+products.itemId, Content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["Update_Data"] = "Product has been Updated!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Products products = new Products();
+            HttpResponseMessage response = Client.GetAsync(Base_Url + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                var Convert_data = JsonConvert.DeserializeObject<Products>(data);
+                if(Convert_data != null)
+                {
+                    products = Convert_data;
+                    
+                }
+            }
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            Products products = new Products();
+            HttpResponseMessage response = Client.GetAsync(Base_Url + id).Result;
+            if (response.IsSuccessStatusCode) 
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Products>(result);
+                if(data != null) 
+                {
+                    products = data;
+                }
+            }
+            return View(products);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult ConfirmedDelete(int id)
+        {
+            Products products = new Products();
+            HttpResponseMessage response = Client.DeleteAsync(Base_Url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["Delete_Data"] = "Product has been Deleted!";
+                return RedirectToAction("Index");
+                
+            }
+            return View();
+        }
+    }
+}
---
+++ CrudApp_AspNetCoreWebAPP/CrudApp_AspNetCoreWebAPP/Models/Products.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudApp_AspNetCoreWebAPP.Models
+{
+    
+
+     public class Products
+     {
+        
+        [DisplayName("Item Id")]
+        public int itemId { get; set; }
+        [Required]
+        [DisplayName("Item Name")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Please Enter Character between 5 and 20.")]
+        public string itemName { get; set; } = null!;
+        [Required]
+        [DisplayName("Item Description")]
+        [StringLength(20, MinimumLength = 5)]
+        public string itemDescription { get; set; } = null!;
+        [Required]
+        [DisplayName("Item Price")]
+        public int itemPrice { get; set; }
+     }
+
+    
+}
